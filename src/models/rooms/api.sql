03b1ce182135973f545b6f1ba08e99fdbd92fb25
Bricks <76704453+Bricks666@users.noreply.github.com>
2022-12-21 11:52:32 +0300

    Добавление более полного запроса в комнаты

--- src/models/rooms/api.sql
+++ src/models/rooms/api.sql
@@ -1,6 +1,95 @@
+CREATE PROCEDURE `GET_ROOMS`(IN `USER_ID` INT UNSIGNED
+) BEGIN
+	SELECT
+	    `ROOMS`.`ID`,
+	    `ROOMS`.`NAME`,
+	    `ROOMS`.`DESCRIPTION`, (
+	        SELECT
+	            COUNT(`GROUPS`.`ID`)
+	        FROM `GROUPS`
+	        WHERE
+	            `GROUPS`.`ROOM_ID` = `ROOMS`.`ID`
+	    ) as `GROUPS_COUNT`, (
+	        SELECT
+	            COUNT(
+	                IF(
+	                    `USER_ROOM`.`DELETED` = 1,
+	                    NULL,
+	                    `USER_ROOM`.`USER_ID`
+	                )
+	            )
+	        FROM `USER_ROOM`
+	        WHERE
+	            `USER_ROOM`.`ROOM_ID` = `ROOMS`.`ID`
+	    ) as `USERS_COUNT`, (
+	        SELECT
+	            COUNT(
+	                IF(
+	                    `TASKS`.`STATUS` != 'DONE',
+	                    NULL,
+	                    `TASKS`.`ID`
+	                )
+	            )
+	        FROM `TASKS`
+	        WHERE
+	            `TASKS`.`ROOM_ID` = `ROOMS`.`ID`
+	    ) as `DONE_TASKS_COUT`, (
+	        SELECT
+	            COUNT(`TASKS`.`ID`)
+	        FROM `TASKS`
+	        WHERE
+	            `TASKS`.`ROOM_ID` = `ROOMS`.`ID`
+	    ) as `TASKS_COUT`
+	FROM `ROOMS`
+	    JOIN `USER_ROOM` ON `USER_ROOM`.`ROOM_ID` = `ROOMS`.`ID` AND `USER_ROOM`.`USER_ID` = `USER_ID` AND `USER_ROOM`.`DELETED` = 0
+	GROUP BY `ROOMS`.`ID`;
+END;
+
 CREATE PROCEDURE `GET_ROOM`(IN `ROOM_ID` INT UNSIGNED
 ) BEGIN
-	SELECT * FROM `ROOMS` WHERE `ID` = `ROOM_ID`;
+	SELECT
+	    `ROOMS`.`ID`,
+	    `ROOMS`.`NAME`,
+	    `ROOMS`.`DESCRIPTION`, (
+	        SELECT
+	            COUNT(`GROUPS`.`ID`)
+	        FROM `GROUPS`
+	        WHERE
+	            `GROUPS`.`ROOM_ID` = `ROOMS`.`ID`
+	    ) as `GROUPS_COUNT`, (
+	        SELECT
+	            COUNT(
+	                IF(
+	                    `USER_ROOM`.`DELETED` = 1,
+	                    NULL,
+	                    `USER_ROOM`.`USER_ID`
+	                )
+	            )
+	        FROM `USER_ROOM`
+	        WHERE
+	            `USER_ROOM`.`ROOM_ID` = `ROOMS`.`ID`
+	    ) as `USERS_COUNT`, (
+	        SELECT
+	            COUNT(
+	                IF(
+	                    `TASKS`.`STATUS` != 'DONE',
+	                    NULL,
+	                    `TASKS`.`ID`
+	                )
+	            )
+	        FROM `TASKS`
+	        WHERE
+	            `TASKS`.`ROOM_ID` = `ROOMS`.`ID`
+	    ) as `DONE_TASKS_COUT`, (
+	        SELECT
+	            COUNT(`TASKS`.`ID`)
+	        FROM `TASKS`
+	        WHERE
+	            `TASKS`.`ROOM_ID` = `ROOMS`.`ID`
+	    ) as `TASKS_COUT`
+	FROM `ROOMS`
+	WHERE `ID` = `ROOM_ID`
+	GROUP BY `ROOMS`.`ID`;
 END;
 
 CREATE PROCEDURE `ADD_ROOM`(IN `NAME` VARCHAR(32),
